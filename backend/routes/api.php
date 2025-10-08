999f9690e4ce59ae89573012f1a961d6f03a885b
AlanCarrillo13 <150700500+AlanCarrillo13@users.noreply.github.com>
2025-10-08 17:10:38 -0600

    Complemento de login y registro de usuario

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ContactController;
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::post('/find-contacts', [ContactController::class, 'findContacts']);
-Route::post('/filter-contacts', [ContactController::class, 'filterContacts']); // <-- nueva ruta
+Route::post('/filter-contacts', [ContactController::class, 'filterContacts']);
 Route::post('/send-message', [MessageController::class, 'sendMessage']);
 Route::post('/send-media', [MessageController::class, 'sendMedia']);
